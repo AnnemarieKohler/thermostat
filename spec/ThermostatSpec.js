d51847cc12ffe7a57713f7a855bc54b5738ba798
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-30 21:50:25 +0100

    added thermostat object and spec

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,9 @@
+describe('Thermostat', function () {
+  var thermostat;
+  
+  it('starts at 20 degrees', function() {
+    thermostat = new Thermostat()
+    expect(thermostat.temperature).toEqual(20);
+  });
+  
+});
