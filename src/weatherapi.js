d16a64fa5b25d2eb66764468e3d3cab42bd974e2
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-01 11:38:48 +0100

    Impletented input field so user can input any city and get weather

--- src/weatherapi.js
+++ src/weatherapi.js
@@ -1,19 +1,33 @@
 $( document ).ready(function() {
+    $('#submitWeather').click(function() {
+      var city = $("#city").val();
 
-  $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    data: {
-      q: "Barcelona,Spain",
-      units: "metric",
-      appid: "28c860d167add3874b5a9c38e0a14f23"
-    },
-    type: "GET",
-    dataType: "json",
-  })
+      $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        data: {
+          q: city,
+          units: "metric",
+          appid: "28c860d167add3874b5a9c38e0a14f23"
+        },
+        type: "GET",
+        dataType: "json",
+      })
 
-  .done(function( json ) {
-    var temp = "The temperature in Barcelona is " + json.main.temp;
-    $(".weather").text(temp);
-  });
+      .done(function( json ) {
+        var temp = "The temperature in " + city + " is " + json.main.temp;
+        $(".weather").text(temp);
+      })
 
+      .fail(function( xhr, status, errorThrown ) {
+        alert( "Ain't no sunshine when she's gone..." );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+      })
+
+      .always(function( xhr, status ) {
+        console.log("Sunshine my only sunshine, you make me happy when skies are grey.🌞");
+
+      });
+    });
 });
